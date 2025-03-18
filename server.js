106ef3c33837be94ca3490d2a4ad44192e29f818
DussaUday <dussauday469@gmail.com>
2025-03-18 21:09:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(cors({
 
 
 
-app.use("/api/auth", authRoutes);
+app.use("/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
