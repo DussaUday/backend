68f002823dba66e3719bbe1a88a3a9fa86253eb0
DussaUday <dussauday469@gmail.com>
2025-03-18 22:19:55 +0530

    Update game.routes.js

--- routes/game.routes.js
+++ routes/game.routes.js
@@ -1,14 +1,14 @@
 import express from "express";
-import protectRoute from "../middleware/protectRoute.js";
+//import protectRoute from "../middleware/protectRoute.js";
 import { sendGameRequest, acceptGameRequest, rejectGameRequest, markCell , getPendingGameRequests} from "../controllers/game.controller.js";
 import { io, userSocketMap } from "../socket/socket.js";
 
 const router = express.Router();
 
-router.post("/send-request", protectRoute, (req, res) => sendGameRequest(req, res, io, userSocketMap));
-router.post("/accept-request", protectRoute, (req, res) => acceptGameRequest(req, res, io, userSocketMap));
-router.post("/reject-request", protectRoute, (req, res) => rejectGameRequest(req, res, io, userSocketMap));
-router.post("/mark-cell", protectRoute, (req, res) => markCell(req, res, io, userSocketMap));
-router.get("/pending-requests", protectRoute, (req, res) => getPendingGameRequests(req, res));
+router.post("/send-request", (req, res) => sendGameRequest(req, res, io, userSocketMap));
+router.post("/accept-request", (req, res) => acceptGameRequest(req, res, io, userSocketMap));
+router.post("/reject-request", (req, res) => rejectGameRequest(req, res, io, userSocketMap));
+router.post("/mark-cell", (req, res) => markCell(req, res, io, userSocketMap));
+router.get("/pending-requests", (req, res) => getPendingGameRequests(req, res));
 
 export default router;
